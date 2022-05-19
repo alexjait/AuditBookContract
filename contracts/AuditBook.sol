d56a6480ea1b84d7f625b5600af69100a24f1c73
Alejandro Jait <alex.jait.altman@gmail.com>
2022-05-18 21:12:12 -0300

    It is added CompanyManager contract

--- contracts/AuditBook.sol
+++ contracts/AuditBook.sol
@@ -2,18 +2,141 @@
 
 pragma solidity ^0.8.0;
 
-contract AuditBook {
-    enum StateType { Pending, Approved, Rejected }
-    enum AuditState { Submitted, Approved, Rejected }
-    enum CompanyType { Audit, Auditable }
+contract CompanyManager {
+    enum RequestAdmissionStateEnum { Pending, Approved, Rejected }
+    enum CompanyTypeEnum { Audit, Auditable }
 
     struct Company {
+        CompanyTypeEnum companyType;
         address account;
         string name;
-        StateType state;
+        RequestAdmissionStateEnum state;
         string changeStateMessage;
-        uint[] auditReference; //Index reference to Audit array (see below)
     }
+
+    address[] internal companiesAddresses;
+    mapping(address => Company) public companies;
+
+    address admin;
+
+    event CompanyStateChanged(string companyName, RequestAdmissionStateEnum actualState);
+
+    constructor(address _admin) {
+        admin = _admin;
+    }
+
+    function getAll() public view returns (Company[] memory) {
+        uint length = companiesAddresses.length;
+        Company[] memory companiesArray = new Company[](length);
+
+        for (uint i = 0; i < length; i++) {
+            companiesArray[i] = companies[companiesAddresses[i]];
+        }
+
+        return companiesArray;
+    }
+
+    function getApproved() external view returns (Company[] memory) {
+        uint length = companiesAddresses.length;
+        uint countApproved = 0;
+
+        for (uint i = 0; i < length; i++) {
+            if (companies[companiesAddresses[i]].state == RequestAdmissionStateEnum.Approved) {
+                countApproved++;
+            }
+        }
+
+        Company[] memory companiesArray = new Company[](countApproved);
+        uint counter = 0;
+
+        for (uint i = 0; i < length && countApproved > 0; i++) {
+            if (companies[companiesAddresses[i]].state == RequestAdmissionStateEnum.Approved) {
+                companiesArray[counter] = companies[companiesAddresses[i]];
+                counter++;
+                countApproved--;
+            }
+        }
+
+        return companiesArray;
+    }
+
+    function CompanyChangeState(address sender, address _company, RequestAdmissionStateEnum _newState, string memory _message) private {
+        require(
+            sender == admin,
+            "You must be the administrator to change state"
+        );
+
+        companies[_company].state = _newState;
+        companies[_company].changeStateMessage = _message;
+        emit CompanyStateChanged(companies[_company].name, _newState);
+    }
+
+    function Approve(address sender, address _company) external {
+        require (
+            companies[_company].account != address(0x0),
+            "The company doesn't have a pending admission"
+        );
+        require (
+            companies[_company].state != RequestAdmissionStateEnum.Approved,
+            "The company is already approved"
+        );
+
+        CompanyChangeState(sender, _company, RequestAdmissionStateEnum.Approved, "You admision was approved");
+    }
+
+    function Reject(address sender, address _company, string memory _message) external {
+        require (
+            companies[_company].account != address(0x0),
+            "The company doesn't have a pending admission"
+        );
+        require (
+            companies[_company].state != RequestAdmissionStateEnum.Rejected,
+            "The company is already rejected"
+        );
+
+        CompanyChangeState(sender, _company, RequestAdmissionStateEnum.Rejected, _message);
+    }
+
+    function IsRegistered(address sender) external view returns (bool) {
+        return companies[sender].account != address(0x0);
+    }
+
+    function RequestAdmision(address sender, string memory _companyName, CompanyTypeEnum companyType) external {
+        require (
+            companies[sender].account == address(0x0),
+            "The company is already registered"
+        );
+
+        RequestAdmissionStateEnum currentState = RequestAdmissionStateEnum.Pending;
+        companies[sender] = Company(companyType, sender, _companyName, currentState, "");
+        companiesAddresses.push(sender);
+        emit CompanyStateChanged(_companyName, currentState);
+    }
+
+    function CompanyState(address sender) external view returns (RequestAdmissionStateEnum) {
+        require (
+            companies[sender].account != address(0x0),
+            "The company is not registered"
+        );
+
+        return companies[sender].state;
+    }
+
+    function getCompanyRegister(address sender) external view returns (Company memory) {
+        require (
+            companies[sender].account != address(0x0),
+            "The company is not registered"
+        );
+
+        return companies[sender];
+    }
+}
+
+contract AuditBook {
+    CompanyManager auditCompanyManager;
+    CompanyManager auditableCompanyManager;
+
+    enum AuditState { Submitted, Approved, Rejected }
 
     struct Audit {
         uint id;
@@ -26,21 +149,18 @@ contract AuditBook {
     address public admin;
     string public adminName;
 
-    address[] auditCompaniesAddresses;
-    address[] auditableCompaniesAddresses;
-    mapping(address => Company) public auditCompanies;
-    mapping(address => Company) public auditableCompanies;
-
     uint auditsCounter;
     Audit[] audits;
+    mapping(address => uint[]) auditCompanyAuditsReference;
+    mapping(address => uint[]) auditableCompanyAuditsReference;
 
-    event AuditCompanyStateChanged(string companyName, StateType actualState);
-    event AuditableCompanyStateChanged(string companyName, StateType actualState);
     event AuditSubmitted(Audit audit);
     event AuditSubmittedStateChanged(Audit audit);
 
     constructor() {
         admin = msg.sender;
+        auditCompanyManager = new CompanyManager(admin);
+        auditableCompanyManager = new CompanyManager(admin);
         auditsCounter = 0;
     }
 
@@ -54,164 +174,63 @@ contract AuditBook {
         adminName = _name;
     }
 
-    function getAuditCompanies() external view returns (Company[] memory) {
-        uint length = auditCompaniesAddresses.length;
-        Company[] memory companies = new Company[](length);
-
-        for (uint i = 0; i < length; i++) {
-            companies[i] = auditCompanies[auditCompaniesAddresses[i]];
-        }
-
-        return companies;
+    function getAuditCompanies() external view returns (CompanyManager.Company[] memory) {
+        return auditCompanyManager.getAll();
     }
 
-    function getAuditableCompanies() external view returns (Company[] memory) {
-        uint length = auditableCompaniesAddresses.length;
-        Company[] memory companies = new Company[](length);
-
-        for (uint i = 0; i < length; i++) {
-            companies[i] = auditableCompanies[auditableCompaniesAddresses[i]];
-        }
-
-        return companies;
+    function getAuditableCompanies() external view returns (CompanyManager.Company[] memory) {
+        return auditableCompanyManager.getAll();
     }
 
-    function getApprovedAuditableCompanies() external view returns (Company[] memory) {
-        uint length = auditableCompaniesAddresses.length;
-        uint countApproved = 0;
-
-        for (uint i = 0; i < length; i++) {
-            if (auditableCompanies[auditableCompaniesAddresses[i]].state == StateType.Approved) {
-                countApproved++;
-            }
-        }
-
-        Company[] memory companies = new Company[](countApproved);
-        uint counter = 0;
-
-        for (uint i = 0; i < length && countApproved > 0; i++) {
-            if (auditableCompanies[auditableCompaniesAddresses[i]].state == StateType.Approved) {
-                companies[counter] = auditableCompanies[auditableCompaniesAddresses[i]];
-                counter++;
-                countApproved--;
-            }
-        }
-
-        return companies;
-    }
-
-    function CompanyChangeState(CompanyType _companyType, address _company, StateType _newState, string memory _message) internal {
-        require(
-            msg.sender == admin,
-            "You must be the administrator to change state"
-        );
-
-        if (_companyType == CompanyType.Audit) {
-            auditCompanies[_company].state = _newState;
-            auditCompanies[_company].changeStateMessage = _message;
-            emit AuditCompanyStateChanged(auditCompanies[_company].name, _newState);
-        } else {
-            auditableCompanies[_company].state = _newState;
-            auditableCompanies[_company].changeStateMessage = _message;
-            emit AuditableCompanyStateChanged(auditableCompanies[_company].name, _newState);
-        }
+    function getApprovedAuditableCompanies() external view returns (CompanyManager.Company[] memory) {
+        return auditableCompanyManager.getApproved();
     }
 
     function ApproveAuditCompany(address _company) external {
-        require (
-            auditCompanies[_company].account != address(0x0),
-            "The company doesn't have a pending admission"
-        );
-        require (
-            auditCompanies[_company].state != StateType.Approved,
-            "The company is already approved"
-        );
-
-        CompanyChangeState(CompanyType.Audit, _company, StateType.Approved, "You admision was approved");
+        return auditCompanyManager.Approve(msg.sender, _company);
     }
 
     function RejectAuditCompany(address _company, string memory _message) external {
-        require (
-            auditCompanies[_company].account != address(0x0),
-            "The company doesn't have a pending admission"
-        );
-        require (
-            auditCompanies[_company].state != StateType.Rejected,
-            "The company is already rejected"
-        );
-
-        CompanyChangeState(CompanyType.Audit, _company, StateType.Rejected, _message);
+        return auditCompanyManager.Reject(msg.sender, _company, _message);
     }
 
     function ApproveAuditableCompany(address _company) external {
-        require (
-            auditableCompanies[_company].account != address(0x0),
-            "The company doesn't have a pending admission"
-        );
-        require (
-            auditableCompanies[_company].state != StateType.Approved,
-            "The company is already rejected"
-        );
-
-        CompanyChangeState(CompanyType.Auditable, _company, StateType.Approved, "You admision was approved");
+        return auditableCompanyManager.Approve(msg.sender, _company);
     }
 
     function RejectAuditableCompany(address _company, string memory _message) external {
-        require (
-            auditableCompanies[_company].account != address(0x0),
-            "The company doesn't have a pending admission"
-        );
-        require (
-            auditableCompanies[_company].state != StateType.Rejected,
-            "The company is already rejected"
-        );
-
-        CompanyChangeState(CompanyType.Auditable, _company, StateType.Rejected, _message);
+        return auditableCompanyManager.Reject(msg.sender, _company, _message);
     }
 
     /* Auditing Companies */
 
     function IsRegisteredAsAuditCompany() external view returns (bool) {
-        return auditCompanies[msg.sender].account != address(0x0);
+        return auditCompanyManager.IsRegistered(msg.sender);
     }
 
     function AuditCompanyRequestAdmision(string memory _companyName) external {
-        require (
-            auditCompanies[msg.sender].account == address(0x0),
-            "The company is already registered"
-        );
-        
-        StateType currentState = StateType.Pending;
-        uint[] memory auditReference;
-        auditCompanies[msg.sender] = Company(msg.sender, _companyName, currentState, "", auditReference);
-        auditCompaniesAddresses.push(msg.sender);
-        emit AuditCompanyStateChanged(_companyName, currentState);
+        return auditCompanyManager.RequestAdmision(msg.sender, _companyName, CompanyManager.CompanyTypeEnum.Audit);
     }
 
-    function AuditCompanyState() external view returns (StateType) {
-        require (
-            auditCompanies[msg.sender].account != address(0x0),
-            "The company is not registered"
-        );
-
-        return auditCompanies[msg.sender].state;
+    function AuditCompanyState() external view returns (CompanyManager.RequestAdmissionStateEnum) {
+        return auditCompanyManager.CompanyState(msg.sender);
     }
 
     function SubmitAudit(address _auditableCompany, string memory _finding) external {
         require (
-            auditCompanies[msg.sender].account != address(0x0),
+            auditCompanyManager.IsRegistered(msg.sender),
             "The audit company is not registered"
         );
         require (
-            auditableCompanies[_auditableCompany].account != address(0x0),
+            auditableCompanyManager.IsRegistered(_auditableCompany),
             "The auditable company is not registered"
         );
         require (
-            auditCompanies[msg.sender].state == StateType.Approved,
+            auditCompanyManager.CompanyState(msg.sender) == CompanyManager.RequestAdmissionStateEnum.Approved,
             "The audit company is not approved"
         );
         require (
-            auditableCompanies[_auditableCompany].state == StateType.Approved,
+            auditableCompanyManager.CompanyState(_auditableCompany) == CompanyManager.RequestAdmissionStateEnum.Approved,
             "The auditable company is not approved"
         );
         require (
@@ -228,70 +247,44 @@ contract AuditBook {
                 AuditState.Submitted, 
                 _finding));
 
-        auditCompanies[msg.sender].auditReference.push(auditsCounter - 1);
-        auditableCompanies[_auditableCompany].auditReference.push(auditsCounter - 1);
+        auditCompanyAuditsReference[msg.sender].push(auditsCounter - 1);
+        auditableCompanyAuditsReference[_auditableCompany].push(auditsCounter - 1);
         emit AuditSubmitted(audits[auditsCounter - 1]);
     }
 
-    function getAuditCompanyRegister() external view returns (Company memory) {
-        require (
-            auditCompanies[msg.sender].account != address(0x0),
-            "The audit company is not registered"
-        );
-
-        return auditCompanies[msg.sender];
+    function getAuditCompanyRegister() external view returns (CompanyManager.Company memory) {
+        return auditCompanyManager.getCompanyRegister(msg.sender);
     }
 
     /* Auditable Companies */
 
     function IsRegisteredAsAuditableCompany() external view returns (bool) {
-        return auditableCompanies[msg.sender].account != address(0x0);
+        return auditableCompanyManager.IsRegistered(msg.sender);
     }
 
     function AuditableCompanyRequestAdmision(string memory _companyName) external {
-        require (
-            auditableCompanies[msg.sender].account == address(0x0),
-            "The company is already registered"
-        );
-        
-        StateType currentState = StateType.Pending;
-        uint[] memory auditReference;
-        auditableCompanies[msg.sender] = Company(msg.sender, _companyName, currentState, "", auditReference);
-        auditableCompaniesAddresses.push(msg.sender);
-        emit AuditableCompanyStateChanged(_companyName, currentState);
+        auditableCompanyManager.RequestAdmision(msg.sender, _companyName, CompanyManager.CompanyTypeEnum.Auditable);
     }
 
-    function AuditableCompanyState() external view returns (StateType) {
-        require (
-            auditableCompanies[msg.sender].account != address(0x0),
-            "The company is not registered"
-        );
-
-        return auditableCompanies[msg.sender].state;
+    function AuditableCompanyState() external view returns (CompanyManager.RequestAdmissionStateEnum) {
+        return auditableCompanyManager.CompanyState(msg.sender);
     }
 
-    function getAuditableCompanyRegister() external view returns (Company memory) {
-        require (
-            auditableCompanies[msg.sender].account != address(0x0),
-            "The audit company is not registered"
-        );
-
-        return auditableCompanies[msg.sender];
+    function getAuditableCompanyRegister() external view returns (CompanyManager.Company memory) {
+        return auditableCompanyManager.getCompanyRegister(msg.sender);
     }
 
     function getAuditableCompanySubmittedAudits() external view returns (Audit[] memory) {
         require (
-            auditableCompanies[msg.sender].account != address(0x0),
-            "The audit company is not registered"
+            auditableCompanyManager.IsRegistered(msg.sender),
+            "The auditable company is not registered"
         );
         require (
-            auditableCompanies[msg.sender].state == StateType.Approved,
+            auditableCompanyManager.CompanyState(msg.sender) == CompanyManager.RequestAdmissionStateEnum.Approved,
             "The auditable company is not approved"
         );
 
-        Company storage company = auditableCompanies[msg.sender];
-        uint[] storage ids = company.auditReference;
-        
+        uint[] storage ids = auditableCompanyAuditsReference[msg.sender];
         uint countPending = 0;
 
         for (uint i = 0; i < ids.length; i++) {
@@ -316,11 +309,11 @@ contract AuditBook {
 
     function AuditableCompanyApproveSubmittedAudit(uint id) external {
         require (
-            auditableCompanies[msg.sender].account != address(0x0),
-            "The audit company is not registered"
+            auditableCompanyManager.IsRegistered(msg.sender),
+            "The auditable company is not registered"
         );
         require (
-            auditableCompanies[msg.sender].state == StateType.Approved,
+            auditableCompanyManager.CompanyState(msg.sender) == CompanyManager.RequestAdmissionStateEnum.Approved,
             "The auditable company is not approved"
         );
         require (
@@ -330,6 +323,10 @@ contract AuditBook {
         require (
             audits[id].state == AuditState.Submitted,
             "The audit is already approved o rejected"
+        );
+        require(
+            audits[id].auditableCompanies == msg.sender,
+            "The audit belongs to another company"
         );
 
         audits[id - 1].state = AuditState.Approved;
@@ -338,11 +335,11 @@ contract AuditBook {
 
     function AuditableCompanyRejectSubmittedAudit(uint id) external {
         require (
-            auditableCompanies[msg.sender].account != address(0x0),
-            "The audit company is not registered"
+            auditableCompanyManager.IsRegistered(msg.sender),
+            "The auditable company is not registered"
         );
         require (
-            auditableCompanies[msg.sender].state == StateType.Approved,
+            auditableCompanyManager.CompanyState(msg.sender) == CompanyManager.RequestAdmissionStateEnum.Approved,
             "The auditable company is not approved"
         );
         require (
@@ -352,6 +349,10 @@ contract AuditBook {
         require (
             audits[id].state == AuditState.Submitted,
             "The audit is already approved o rejected"
+        );
+        require(
+            audits[id].auditableCompanies == msg.sender,
+            "The audit belongs to another company"
         );
 
         audits[id - 1].state = AuditState.Rejected;
